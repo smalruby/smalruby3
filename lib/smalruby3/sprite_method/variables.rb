303e022cf0c730e04cf295de53e868b4c1aa1bc5
Kouji Takao <kouji.takao@gmail.com>
2018-11-02 22:04:06 +0900

    added variables method, but now only raise NotImplementedError.

--- lib/smalruby3/sprite_method/variables.rb
+++ lib/smalruby3/sprite_method/variables.rb
@@ -2,6 +2,21 @@ module Smalruby3
   module SpriteMethod
     # Variables category methods
     module Variables
+      def show_variable(name)
+        raise NotImplementedError, "not implemented: show_variable(#{name.inspect})"
+      end
+
+      def hide_variable(name)
+        raise NotImplementedError, "not implemented: hide_variable(#{name.inspect})"
+      end
+
+      def show_list(name)
+        raise NotImplementedError, "not implemented: show_list(#{name.inspect})"
+      end
+
+      def hide_list(name)
+        raise NotImplementedError, "not implemented: hide_list(#{name.inspect})"
+      end
     end
   end
 end
