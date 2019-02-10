3c425a554daa2f26c1aa452d9962b9953d70e7f2
T_Shun <shirokumasun42@gmail.com>
2019-02-10 13:51:14 +0900

    sayメソッドの修正

--- lib/smalruby3/sprite_method/looks.rb
+++ lib/smalruby3/sprite_method/looks.rb
@@ -2,6 +2,12 @@ module Smalruby3
   module SpriteMethod
     # Looks category methods
     module Looks
+      def say(message, second = nil)
+      	#コンソール上にmessageの表示
+      	puts message
+      end
     end
   end
 end
+
+
