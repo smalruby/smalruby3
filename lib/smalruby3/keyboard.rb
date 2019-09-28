1f093179dbd4173331f2695c5a1a5c35bfe4f221
stonesaw <ishichan1260@gmail.com>
2019-09-28 16:00:59 +0900

    リファクタリング

--- lib/smalruby3/keyboard.rb
+++ lib/smalruby3/keyboard.rb
@@ -2,34 +2,27 @@ module Smalruby3
   class Keyboard
     class << self
       def pressed?(key)
-        smalruby_key = key
-        if (smalruby_key == "up arrow")
-          dxruby_key = DXRuby::K_UP
-        end
-        if (smalruby_key == "down arrow")
-          dxruby_key = DXRuby::K_DOWN
-        end
-        if (smalruby_key == "right arrow")
-          dxruby_key = DXRuby::K_RIGHT
-        end
-        if (smalruby_key == "left arrow")
-          dxruby_key = DXRuby.const_get("K_LEFT")
-        end
-        if (smalruby_key == "space")
-          dxruby_key = DXRuby.const_get("K_SPACE")
-        end
-        smalruby_key == "a"
-        if /^[a-z]$/ =~ smalruby_key
-          dxruby_key = DXRuby.const_get("K_" + smalruby_key.upcase)
-        end
-        if /[0-9]/ =~ smalruby_key
-          dxruby_key = DXRuby.const_get("K_" + smalruby_key)
-        end
-        if  (smalruby_key == "any")
+        if key == "any"
           !Input.keys.empty?
         else
-          Input.key_down?(dxruby_key)
+          Input.key_down?(to_dxruby_key(key))
         end
+      end
+
+      def to_dxruby_key(key)
+        if key == "space"
+          return DXRuby.const_get("K_SPACE")
+        end
+        if /^(up|down|right|left) arrow$/ =~ key
+          return DXRuby.const_get("K_" + $1.upcase)
+        end
+        if /^[a-z]$/ =~ key
+          return DXRuby.const_get("K_" + key.upcase)
+        end
+        if /[0-9]/ =~ key
+          return DXRuby.const_get("K_" + key)
+        end
+        raise ArgumentError.new("invalid key: #{key}")
       end
     end
   end
