11cfcbe301cedefe6007f76a5cc375a8127a53d5
riversunlight <aabbee107@icloud.com>
2019-10-04 18:10:13 +0900

    コメントを消しました。

--- lib/smalruby3/sprite_method/sensing.rb
+++ lib/smalruby3/sprite_method/sensing.rb
@@ -2,24 +2,22 @@ module Smalruby3
   module SpriteMethod
     module Sensing
       def touching?(object) 
-      case (object)
-      when "_edge_"
-         
+        case (object)
+        when "_edge_"
           x <= SmalrubyToDXRuby::SCREEN_LEFT || x >= SmalrubyToDXRuby::SCREEN_RIGHT ||
             y <= SmalrubyToDXRuby::SCREEN_BOTTOM || y >= SmalrubyToDXRuby::SCREEN_TOP
-      when "_mouse_"
-   
-        dx_mouse_x = Input.mousePosX
-        dx_mouse_y = Input.mousePosY
-        mouse_x, mouse_y = *dx2s.position(dx_mouse_x, dx_mouse_y)
-        x + costume.width/2 >= mouse_x  && x - costume.width/2 <= mouse_x &&
-          y - costume.height/2 <= mouse_y && y + costume.height/2 >= mouse_y
-      else
-        if !sprite(object)
-             raise ArgumentError, "invalid object: #{object}"
+        when "_mouse_"
+          dx_mouse_x = Input.mousePosX
+          dx_mouse_y = Input.mousePosY
+          mouse_x, mouse_y = *dx2s.position(dx_mouse_x, dx_mouse_y)
+          x + costume.width/2 >= mouse_x  && x - costume.width/2 <= mouse_x &&
+            y - costume.height/2 <= mouse_y && y + costume.height/2 >= mouse_y
+        else
+          if !sprite(object)
+            raise ArgumentError, "invalid object: #{object}"
+          end
         end
-      end
-      end
+     end
     end
   end
 end
