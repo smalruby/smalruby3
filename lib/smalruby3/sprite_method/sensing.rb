db1a3ad3c9daba86964ab6c3760a576f90b6595b
riversunlight <aabbee107@icloud.com>
2019-10-04 17:41:29 +0900

    マウスがスプライトに触れたかどうかを判断するプログラムを書きました

--- lib/smalruby3/sprite_method/sensing.rb
+++ lib/smalruby3/sprite_method/sensing.rb
@@ -8,13 +8,12 @@ module Smalruby3
           x <= SmalrubyToDXRuby::SCREEN_LEFT || x >= SmalrubyToDXRuby::SCREEN_RIGHT ||
             y <= SmalrubyToDXRuby::SCREEN_BOTTOM || y >= SmalrubyToDXRuby::SCREEN_TOP
       when "_mouse_"
-          dx_mouse_x = Input.mousePosX
-          dx_mouse_y = Input.mousePosY
-          mouse_x, mouse_y = *dx2s.position(dx_mouse_x, dx_mouse_y)
-          puts "(#{mouse_x}, #{mouse_y}), (#{x}, #{y}) [#{costume.width},#{costume.height}][#{costume.width/2}][#{x + costume.width/2 <= mouse_x}, #{x - costume.width/2 >= mouse_x}, #{y - costume.height/2 >= mouse_y}, #{y + costume.height/2 <= mouse_y}][#{}]"
-          x + costume.width/2 >= mouse_x  && x - costume.width/2 <= mouse_x &&
-            y - costume.height/2 <= mouse_y && y + costume.height/2 >= mouse_y
-      
+   
+        dx_mouse_x = Input.mousePosX
+        dx_mouse_y = Input.mousePosY
+        mouse_x, mouse_y = *dx2s.position(dx_mouse_x, dx_mouse_y)
+        x + costume.width/2 >= mouse_x  && x - costume.width/2 <= mouse_x &&
+          y - costume.height/2 <= mouse_y && y + costume.height/2 >= mouse_y
       else
         if !sprite(object)
              raise ArgumentError, "invalid object: #{object}"
